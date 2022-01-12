95fecf4cdb9b1614fdee1554fb9093d2b2d300e2
daem0nc0re <daem0nc0re@outlook.com>
2022-01-12 22:38:53 +0900

    Added new tool named as SwitchPriv

--- SwitchPriv/SwitchPriv/SwitchPriv.cs
+++ SwitchPriv/SwitchPriv/SwitchPriv.cs
@@ -9,9 +9,9 @@ namespace SwitchPriv
             CommandLineParser options = new CommandLineParser();
             options.SetTitle("SwitchPriv - Tool to control token privileges.");
             options.Add(false, "h", "help", false, "Displays this help message.");
-            options.Add(false, "e", "enable", string.Empty, "Specifies privilege to enable. Case insensitive.");
-            options.Add(false, "d", "disable", string.Empty, "Specifies privilege to disable. Case insensitive.");
-            options.Add(false, "p", "pid", string.Empty, "Specifies the target PID. Default specifies PPID");
+            options.Add(false, "e", "enable", string.Empty, "Specifies token privilege to enable. Case insensitive.");
+            options.Add(false, "d", "disable", string.Empty, "Specifies token privilege to disable. Case insensitive.");
+            options.Add(false, "p", "pid", string.Empty, "Specifies the target PID. Default specifies PPID.");
             options.Add(false, "l", "list", false, "List values for --enable or --disable option.");
             options.Parse(args);
 
