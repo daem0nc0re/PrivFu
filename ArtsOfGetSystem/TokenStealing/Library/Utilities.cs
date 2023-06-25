853f007a618a655f2f2e968a588d40f86d710247
daem0nc0re <daem0nc0re@outlook.com>
2023-06-25 16:16:32 +0900

    Updated ImpersonateThreadToken for TokenStealing

--- ArtsOfGetSystem/TokenStealing/Library/Utilities.cs
+++ ArtsOfGetSystem/TokenStealing/Library/Utilities.cs
@@ -202,9 +202,9 @@ namespace TokenStealing.Library
         public static bool ImpersonateThreadToken(IntPtr hImpersonationToken)
         {
             IntPtr pImpersonationLevel = Marshal.AllocHGlobal(4);
-            bool status = NativeMethods.ImpersonateLoggedOnUser(hImpersonationToken);
+            var status = false;
 
-            if (status)
+            if (NativeMethods.ImpersonateLoggedOnUser(hImpersonationToken))
             {
                 NTSTATUS ntstatus = NativeMethods.NtQueryInformationToken(
                     WindowsIdentity.GetCurrent().Token,
@@ -212,9 +212,8 @@ namespace TokenStealing.Library
                     pImpersonationLevel,
                     4u,
                     out uint _);
-                status = (ntstatus == Win32Consts.STATUS_SUCCESS);
 
-                if (status)
+                if (ntstatus == Win32Consts.STATUS_SUCCESS)
                 {
                     var level = (SECURITY_IMPERSONATION_LEVEL)Marshal.ReadInt32(pImpersonationLevel);
 
