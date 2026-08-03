4460545c746dbfd3fae53ae1c8dcb2c53f6fa036
daem0nc0re <daem0nc0re@outlook.com>
2026-08-03 10:54:12 +0900

    Coding manner fix

--- ArtsOfGetSystem/SystemServiceExec/Library/Globals.cs
+++ ArtsOfGetSystem/SystemServiceExec/Library/Globals.cs
@@ -8,8 +8,8 @@ namespace SystemServiceExec.Library
 {
     internal class Globals
     {
-        public static string ServiceName = "SystemServiceExecSvc";
-        public static bool IsServiceCreated = false;
-        public static bool IsServiceDeleted = false;
+        public static string ServiceName { get; } = "SystemServiceExecSvc";
+        public static bool IsServiceCreated { get; set; } = false;
+        public static bool IsServiceDeleted { get; set; } = false;
     }
 }
