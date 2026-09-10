4f2b5bfa9cef9f75312d7d03099c8eaa4221b6f8
daem0nc0re <daem0nc0re@outlook.com>
2026-09-10 11:12:51 +0900

    Remove unused code

--- ArtsOfGetSystem/NamedPipeImpersonation/Interop/Win32Enums.cs
+++ ArtsOfGetSystem/NamedPipeImpersonation/Interop/Win32Enums.cs
@@ -31,22 +31,6 @@ namespace NamedPipeImpersonation.Interop
         PROCESS_SET_LIMITED_INFORMATION = 0x00002000,
         PROCESS_ALL_ACCESS = 0x001FFFFF,
 
-        // For Thread
-        THREAD_TERMINATE = 0x00000001,
-        THREAD_SUSPEND_RESUME = 0x00000002,
-        THREAD_ALERT = 0x00000004,
-        THREAD_GET_CONTEXT = 0x00000008,
-        THREAD_SET_CONTEXT = 0x00000010,
-        THREAD_SET_INFORMATION = 0x00000020,
-        THREAD_QUERY_INFORMATION = 0x00000040,
-        THREAD_SET_THREAD_TOKEN = 0x00000080,
-        THREAD_IMPERSONATE = 0x00000100,
-        THREAD_DIRECT_IMPERSONATION = 0x00000200,
-        THREAD_SET_LIMITED_INFORMATION = 0x00000400,
-        THREAD_QUERY_LIMITED_INFORMATION = 0x00000800,
-        THREAD_RESUME = 0x00001000,
-        THREAD_ALL_ACCESS = 0x001FFFFF,
-
         // For Token
         TOKEN_ASSIGN_PRIMARY = 0x00000001,
         TOKEN_DUPLICATE = 0x00000002,
@@ -61,37 +45,6 @@ namespace NamedPipeImpersonation.Interop
         TOKEN_EXECUTE = 0x00020000,
         TOKEN_READ = 0x00020008,
         TOKEN_WRITE = 0x000200E0,
-
-        // For Files
-        FILE_ANY_ACCESS = 0x00000000,
-        FILE_READ_ACCESS = 0x00000001,
-        FILE_WRITE_ACCESS = 0x00000002,
-        FILE_READ_DATA = 0x00000001,
-        FILE_LIST_DIRECTORY = 0x00000001,
-        FILE_WRITE_DATA = 0x00000002,
-        FILE_ADD_FILE = 0x00000002,
-        FILE_APPEND_DATA = 0x00000004,
-        FILE_ADD_SUBDIRECTORY = 0x00000004,
-        FILE_CREATE_PIPE_INSTANCE = 0x00000004,
-        FILE_READ_EA = 0x00000008,
-        FILE_WRITE_EA = 0x00000010,
-        FILE_EXECUTE = 0x00000020,
-        FILE_TRAVERSE = 0x00000020,
-        FILE_DELETE_CHILD = 0x00000040,
-        FILE_READ_ATTRIBUTES = 0x00000080,
-        FILE_WRITE_ATTRIBUTES = 0x00000100,
-        FILE_ALL_ACCESS = 0x001F01FF,
-        FILE_GENERIC_READ = 0x00100089,
-        FILE_GENERIC_WRITE = 0x00100116,
-        FILE_GENERIC_EXECUTE = 0x001000A0,
-
-        // Job
-        JOB_ASSIGN_PROCESS = 0x00000001,
-        JOB_SET_ATTRIBUTES = 0x00000002,
-        JOB_QUERY = 0x00000004,
-        JOB_TERMINATE = 0x00000008,
-        JOB_SET_SECURITY_ATTRIBUTES = 0x00000010,
-        JOB_IMPERSONATE = 0x00000020,
 
         // Service Control Manager
         SC_MANAGER_CONNECT = 0x00000001,
@@ -131,48 +84,7 @@ namespace NamedPipeImpersonation.Interop
         GENERIC_ALL = 0x10000000,
         GENERIC_EXECUTE = 0x20000000,
         GENERIC_WRITE = 0x40000000,
-        GENERIC_READ = 0x80000000,
-        DESKTOP_READOBJECTS = 0x00000001,
-        DESKTOP_CREATEWINDOW = 0x00000002,
-        DESKTOP_CREATEMENU = 0x00000004,
-        DESKTOP_HOOKCONTROL = 0x00000008,
-        DESKTOP_JOURNALRECORD = 0x00000010,
-        DESKTOP_JOURNALPLAYBACK = 0x00000020,
-        DESKTOP_ENUMERATE = 0x00000040,
-        DESKTOP_WRITEOBJECTS = 0x00000080,
-        DESKTOP_SWITCHDESKTOP = 0x00000100,
-        WINSTA_ENUMDESKTOPS = 0x00000001,
-        WINSTA_READATTRIBUTES = 0x00000002,
-        WINSTA_ACCESSCLIPBOARD = 0x00000004,
-        WINSTA_CREATEDESKTOP = 0x00000008,
-        WINSTA_WRITEATTRIBUTES = 0x00000010,
-        WINSTA_ACCESSGLOBALATOMS = 0x00000020,
-        WINSTA_EXITWINDOWS = 0x00000040,
-        WINSTA_ENUMERATE = 0x00000100,
-        WINSTA_READSCREEN = 0x00000200,
-        WINSTA_ALL_ACCESS = 0x0000037F,
-
-        // For section
-        SECTION_QUERY = 0x00000001,
-        SECTION_MAP_WRITE = 0x00000002,
-        SECTION_MAP_READ = 0x00000004,
-        SECTION_MAP_EXECUTE = 0x00000008,
-        SECTION_EXTEND_SIZE = 0x00000010,
-        SECTION_MAP_EXECUTE_EXPLICIT = 0x00000020,
-        SECTION_ALL_ACCESS = 0x000F001F
-    }
-
-    internal enum COMPUTER_NAME_FORMAT
-    {
-        ComputerNameNetBIOS,
-        ComputerNameDnsHostname,
-        ComputerNameDnsDomain,
-        ComputerNameDnsFullyQualified,
-        ComputerNamePhysicalNetBIOS,
-        ComputerNamePhysicalDnsHostname,
-        ComputerNamePhysicalDnsDomain,
-        ComputerNamePhysicalDnsFullyQualified,
-        ComputerNameMax
+        GENERIC_READ = 0x80000000
     }
 
     internal enum ERROR_CONTROL
@@ -192,99 +104,6 @@ namespace NamedPipeImpersonation.Interop
         FORMAT_MESSAGE_FROM_HMODULE = 0x00000800,
         FORMAT_MESSAGE_FROM_SYSTEM = 0x00001000,
         FORMAT_MESSAGE_ARGUMENT_ARRAY = 0x00002000
-    }
-
-    [Flags]
-    internal enum JOB_OBJECT_LIMIT : uint
-    {
-        WORKINGSET = 0x00000001,
-        PROCESS_TIME = 0x00000002,
-        JOB_TIME = 0x00000004,
-        ACTIVE_PROCESS = 0x00000008,
-        AFFINITY = 0x00000010,
-        PRIORITY_CLASS = 0x00000020,
-        PRESERVE_JOB_TIME = 0x00000040,
-        SCHEDULING_CLASS = 0x00000080,
-        PROCESS_MEMORY = 0x00000100,
-        JOB_MEMORY = 0x00000200,
-        DIE_ON_UNHANDLED_EXCEPTION = 0x00000400,
-        BREAKAWAY_OK = 0x00000800,
-        SILENT_BREAKAWAY_OK = 0x00001000,
-        KILL_ON_JOB_CLOSE = 0x00002000,
-        SUBSET_AFFINITY = 0x00004000,
-        JOB_MEMORY_LOW = 0x00008000,
-        JOB_READ_BYTES = 0x00010000,
-        JOB_WRITE_BYTES = 0x00020000,
-        RATE_CONTROL = 0x00040000,
-        IO_RATE_CONTROL = 0x00080000,
-        NET_RATE_CONTROL = 0x00100000
-    }
-
-    internal enum JOBOBJECTINFOCLASS
-    {
-        JobObjectBasicAccountingInformation = 1, // JOBOBJECT_BASIC_ACCOUNTING_INFORMATION
-        JobObjectBasicLimitInformation = 2, // JOBOBJECT_BASIC_LIMIT_INFORMATION
-        JobObjectBasicProcessIdList = 3, // JOBOBJECT_BASIC_PROCESS_ID_LIST
-        JobObjectBasicUIRestrictions = 4, // JOBOBJECT_BASIC_UI_RESTRICTIONS
-        JobObjectSecurityLimitInformation = 5, // JOBOBJECT_SECURITY_LIMIT_INFORMATION
-        JobObjectEndOfJobTimeInformation = 6, // JOBOBJECT_END_OF_JOB_TIME_INFORMATION
-        JobObjectAssociateCompletionPortInformation = 7, // JOBOBJECT_ASSOCIATE_COMPLETION_PORT
-        JobObjectBasicAndIoAccountingInformation = 8, // JOBOBJECT_BASIC_AND_IO_ACCOUNTING_INFORMATION
-        JobObjectExtendedLimitInformation = 9, // JOBOBJECT_EXTENDED_LIMIT_INFORMATION
-        JobObjectJobSetInformation = 10, // JOBOBJECT_JOBSET_INFORMATION
-        JobObjectGroupInformation = 11, // USHORT
-        JobObjectNotificationLimitInformation = 12, // JOBOBJECT_NOTIFICATION_LIMIT_INFORMATION
-        JobObjectLimitViolationInformation = 13, // JOBOBJECT_LIMIT_VIOLATION_INFORMATION
-        JobObjectGroupInformationEx = 14, // GROUP_AFFINITY (ARRAY)
-        JobObjectCpuRateControlInformation = 15, // JOBOBJECT_CPU_RATE_CONTROL_INFORMATION
-        JobObjectCompletionFilter = 16,
-        JobObjectCompletionCounter = 17,
-        JobObjectFreezeInformation = 18, // JOBOBJECT_FREEZE_INFORMATION
-        JobObjectExtendedAccountingInformation = 19, // JOBOBJECT_EXTENDED_ACCOUNTING_INFORMATION
-        JobObjectWakeInformation = 20, // JOBOBJECT_WAKE_INFORMATION
-        JobObjectBackgroundInformation = 21,
-        JobObjectSchedulingRankBiasInformation = 22,
-        JobObjectTimerVirtualizationInformation = 23,
-        JobObjectCycleTimeNotification = 24,
-        JobObjectClearEvent = 25,
-        JobObjectInterferenceInformation = 26, // JOBOBJECT_INTERFERENCE_INFORMATION
-        JobObjectClearPeakJobMemoryUsed = 27,
-        JobObjectMemoryUsageInformation = 28, // JOBOBJECT_MEMORY_USAGE_INFORMATION // JOBOBJECT_MEMORY_USAGE_INFORMATION_V2
-        JobObjectSharedCommit = 29,
-        JobObjectContainerId = 30,
-        JobObjectIoRateControlInformation = 31,
-        JobObjectNetRateControlInformation = 32, // JOBOBJECT_NET_RATE_CONTROL_INFORMATION
-        JobObjectNotificationLimitInformation2 = 33, // JOBOBJECT_NOTIFICATION_LIMIT_INFORMATION_2
-        JobObjectLimitViolationInformation2 = 34, // JOBOBJECT_LIMIT_VIOLATION_INFORMATION_2
-        JobObjectCreateSilo = 35,
-        JobObjectSiloBasicInformation = 36, // SILOOBJECT_BASIC_INFORMATION
-        JobObjectSiloRootDirectory = 37, // SILOOBJECT_ROOT_DIRECTORY
-        JobObjectServerSiloBasicInformation = 38, // SERVERSILO_BASIC_INFORMATION
-        JobObjectServerSiloUserSharedData = 39, // SILO_USER_SHARED_DATA
-        JobObjectServerSiloInitialize = 40,
-        JobObjectServerSiloRunningState = 41,
-        JobObjectIoAttribution = 42,
-        JobObjectMemoryPartitionInformation = 43,
-        JobObjectContainerTelemetryId = 44,
-        JobObjectSiloSystemRoot = 45,
-        JobObjectEnergyTrackingState = 46, // JOBOBJECT_ENERGY_TRACKING_STATE
-        JobObjectThreadImpersonationInformation = 47,
-        JobObjectIoPriorityLimit = 48,
-        JobObjectPagePriorityLimit = 49,
-        MaxJobObjectInfoClass = 50
-    }
-
-    internal enum MSV1_0_LOGON_SUBMIT_TYPE
-    {
-        MsV1_0InteractiveLogon = 2,
-        MsV1_0Lm20Logon,
-        MsV1_0NetworkLogon,
-        MsV1_0SubAuthLogon,
-        MsV1_0WorkstationUnlockLogon = 7,
-        MsV1_0S4ULogon = 12,
-        MsV1_0VirtualLogon = 82,
-        MsV1_0NoElevationLogon = 83,
-        MsV1_0LuidLogon = 84
     }
 
     [Flags]
@@ -345,20 +164,6 @@ namespace NamedPipeImpersonation.Interop
     }
 
     [Flags]
-    internal enum SE_GROUP_ATTRIBUTES : uint
-    {
-        Mandatory = 0x00000001,
-        EnabledByDefault = 0x00000002,
-        Enabled = 0x00000004,
-        Owner = 0x00000008,
-        UseForDenyOnly = 0x00000010,
-        Integrity = 0x00000020,
-        IntegrityEnabled = 0x00000040,
-        Resource = 0x20000000,
-        LogonId = 0xC0000000
-    }
-
-    [Flags]
     internal enum SE_PRIVILEGE_ATTRIBUTES : uint
     {
         EnabledByDefault = 0x00000001,
@@ -373,23 +178,6 @@ namespace NamedPipeImpersonation.Interop
         SecurityIdentification,
         SecurityImpersonation,
         SecurityDelegation
-    }
-
-    internal enum SECURITY_LOGON_TYPE
-    {
-        UndefinedLogonType = 0,
-        Interactive = 2,
-        Network,
-        Batch,
-        Service,
-        Proxy,
-        Unlock,
-        NetworkCleartext,
-        NewCredentials,
-        RemoteInteractive,
-        CachedInteractive,
-        CachedRemoteInteractive,
-        CachedUnlock
     }
 
     internal enum SERVICE_TYPE
@@ -484,51 +272,5 @@ namespace NamedPipeImpersonation.Interop
     {
         TokenPrimary = 1,
         TokenImpersonation
-    }
-
-    [Flags]
-    internal enum USER_FLAGS : uint
-    {
-        UF_SCRIPT = 0x00000001,
-        UF_ACCOUNTDISABLE = 0x00000002,
-        UF_HOMEDIR_REQUIRED = 0x00000008,
-        UF_LOCKOUT = 0x00000010,
-        UF_PASSWD_NOTREQD = 0x00000020,
-        UF_PASSWD_CANT_CHANGE = 0x00000040,
-        UF_ENCRYPTED_TEXT_PASSWORD_ALLOWED = 0x00000080,
-        UF_TEMP_DUPLICATE_ACCOUNT = 0x00000100,
-        UF_NORMAL_ACCOUNT = 0x00000200,
-        UF_INTERDOMAIN_TRUST_ACCOUNT = 0x00000800,
-        UF_WORKSTATION_TRUST_ACCOUNT = 0x00001000,
-        UF_SERVER_TRUST_ACCOUNT = 0x00002000,
-        UF_DONT_EXPIRE_PASSWD = 0x00010000,
-        UF_MNS_LOGON_ACCOUNT = 0x00020000,
-        UF_SMARTCARD_REQUIRED = 0x00040000,
-        UF_TRUSTED_FOR_DELEGATION = 0x00080000,
-        UF_NOT_DELEGATED = 0x00100000,
-        UF_USE_DES_KEY_ONLY = 0x00200000,
-        UF_DONT_REQUIRE_PREAUTH = 0x00400000,
-        UF_PASSWORD_EXPIRED = 0x00800000,
-        UF_TRUSTED_TO_AUTHENTICATE_FOR_DELEGATION = 0x01000000,
-        UF_NO_AUTH_DATA_REQUIRED = 0x02000000,
-        UF_PARTIAL_SECRETS_ACCOUNT = 0x04000000,
-        UF_USE_AES_KEYS = 0x08000000
-    }
-
-    internal enum USER_INFO_FILTER
-    {
-        InterDomainTrustAccount = 0x8,
-        NormalAccount = 0x2,
-        ProxyAccount = 0x4,
-        ServerTrustAccount = 0x20,
-        TempDuplicateAccount = 0x1,
-        WorkstationTrustAccount = 0x10
-    }
-
-    internal enum USER_PRIVS
-    {
-        Guest,
-        User,
-        Admin
     }
 }
